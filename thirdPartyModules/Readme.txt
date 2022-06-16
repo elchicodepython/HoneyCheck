8ca57da1639a4831e6118088020cc28b80aac19d
Samuel López Saura <samuellopezsaura@gmail.com>
2022-06-16 23:01:29 +0200

    remove thirdPartymodules folder as they will be installed as python packages

--- thirdPartyModules/Readme.txt
+++
@@ -1,4 +0,0 @@
-
-This folder it is supposed to use for adding new non-official modules.
-
-Be free to create your own modules here.
--- thirdPartyModules/__init__.py
+++
@@ -1,6 +0,0 @@
-from os.path import dirname, basename, isfile
-import glob
-
-modules = glob.glob(dirname(__file__) + '/*.py')
-
-__all__ = [basename(fich)[:-3] for fich in modules if isfile(fich)]
